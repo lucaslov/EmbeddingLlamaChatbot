ae0c4f102139042b6f32fc8da64aab3f7e34fe30
lucaslov <czyzukasz2402@gmail.com>
2024-04-20 14:39:20 +0200

    add pdf loader

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
 from dotenv import load_dotenv
+from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader, PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
 PINECONE_KEY = os.getenv('PINECONE_KEY')
-print(PINECONE_KEY)
+loader = PyPDFLoader("../data/summary_strategy.pdf")
+data = loader.load()
+print (f'You have {len(data)} document(s) in your data')
+print (f'There are {len(data[0].page_content)} characters in your document')
